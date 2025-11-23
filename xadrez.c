81f07aa10b5ef4b369f42fa04f3dde315f4298c2
Rafael dos Santos <rafaelmartins_contato@hotmail.com>
2025-11-23 21:21:50 +0000

    feat: implementando as pecas iniciais com os diferente tipos de loops;

--- xadrez.c
+++ xadrez.c
@@ -8,15 +8,40 @@ int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
+   const int bispo = 5;
+   const int rainha = 8;
+   const int torre = 5;
+
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    printf("Bispo se movimentando:\n");
 
+    for (int i = 0; i < bispo; i++) {
+        printf("Cima...\n");
+        printf("Direita\n");
+    }
+    printf("\n");
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
+    printf("Torre se movimentando:\n");
+    
+    int i = 0;
+    
+    while (i < torre) {
+    printf("Direita...\n");
+    i++;
+}
+    printf("\n");
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    
+    printf("Rainha se movimentando:\n");
+    int j = 0;
 
+    do {
+    printf("Esquerda...\n");
+    j++;
+}   while (j < rainha);
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
